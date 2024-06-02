d21d8400df93830cb4e4b5eaa7d559b048e30e48
jsontype <jsontyper@gmail.com>
2024-06-02 16:21:26 +0900

    feat: text change

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,14 +1,14 @@
-"use client";
+"use client"
 
-import React from "react";
-import { motion } from "framer-motion";
+import React from "react"
+import { motion } from "framer-motion"
 import {
   slideInFromLeft,
   slideInFromRight,
   slideInFromTop,
-} from "@/utils/motion";
-import { SparklesIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
+} from "@/utils/motion"
+import { SparklesIcon } from "@heroicons/react/24/solid"
+import Image from "next/image"
 
 const HeroContent = () => {
   return (
@@ -33,10 +33,10 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            FRONTEND
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Yang Donghwa{" "}
             </span>
             project exprience
           </span>
@@ -46,8 +46,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          こんにちは！Webプログラマーの「Azuma」です。韓国から来ました。東京で開発者として働いています。
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
@@ -69,7 +68,7 @@ const HeroContent = () => {
         />
       </motion.div>
     </motion.div>
-  );
-};
+  )
+}
 
-export default HeroContent;
+export default HeroContent
