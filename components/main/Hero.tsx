2e7c01113e4e44357014d9878fb9eb6c25211a87
jsontype <jsontyper@gmail.com>
2024-06-02 16:07:41 +0900

    fix: videoがスクリーンを拡大した時に切れる問題解決

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import HeroContent from "../sub/HeroContent";
+import React from "react"
+import HeroContent from "../sub/HeroContent"
 
 const Hero = () => {
   return (
@@ -8,13 +8,13 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+        className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1]"
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
       <HeroContent />
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
