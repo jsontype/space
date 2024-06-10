1b9716dbb2533491b0fed16d636c943b761a973a
TamakiSaito <immacd1012@gmail.com>
2024-06-10 22:52:27 +0900

    fix: typescript no scrict

--- components/sub/ModalAccordion.tsx
+++ components/sub/ModalAccordion.tsx
@@ -6,6 +6,7 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { SxProps, Theme } from "@mui/material/styles";
 
 interface ModalAccordionProps {
   open: boolean;
@@ -16,16 +17,24 @@ const ModalAccordion: React.FC<ModalAccordionProps> = ({
   open,
   handleClose,
 }) => {
-  const style = {
-    position: "absolute" as "absolute",
+  const positionStyle: SxProps<Theme> = {
+    position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
+  };
+
+  const boxStyle: SxProps<Theme> = {
     width: "50%",
     bgcolor: "black",
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
+  };
+
+  const buttonContainerStyle: SxProps<Theme> = {
+    display: "flex",
+    justifyContent: "flex-end",
   };
 
   return (
@@ -36,60 +45,62 @@ const ModalAccordion: React.FC<ModalAccordionProps> = ({
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-              aria-controls="panel1-content"
-              id="panel1-header"
-              sx={{ bgcolor: "black", color: "white" }}
-            >
-              Q.自己紹介
-            </AccordionSummary>
-            <AccordionDetails>
-              こんにちは!Webプログラマーの「Azuma」です。
-              <br />
-              韓国から来ました。東京で開発者として働いています。
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-              aria-controls="panel1-content"
-              id="panel1-header"
-              sx={{ bgcolor: "black", color: "white" }}
-            >
-              Q.なぜプログラマーですか？
-            </AccordionSummary>
-            <AccordionDetails>
-              私は幼い頃からプログラマーになりたいという夢がありました。
-              <br />
-              また、海外の生活にも憧れを抱いていました。
-              <br />
-              自分がやりたいことを追求しながらも、より社会に貢献し、
-              <br />
-              つまり人間の生活を便利にするプログラムを作ろうと思っております。
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-              aria-controls="panel1-content"
-              id="panel1-header"
-              sx={{ bgcolor: "black", color: "white" }}
-            >
-              Q.強みは何ですか？
-            </AccordionSummary>
-            <AccordionDetails>
-              私の強みは次のとおりです。 <br />
-              ・コーディング:EC、B2C、B2B
-              Saasソリューションなど多様な分野のプロジェクト経験
-            </AccordionDetails>
-          </Accordion>
-          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <Button onClick={handleClose} sx={{ mt: 2 }}>
-              CLOSE
-            </Button>
+        <Box sx={positionStyle}>
+          <Box sx={boxStyle}>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+                sx={{ bgcolor: "black", color: "white" }}
+              >
+                Q.自己紹介
+              </AccordionSummary>
+              <AccordionDetails>
+                こんにちは!Webプログラマーの「Azuma」です。
+                <br />
+                韓国から来ました。東京で開発者として働いています。
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+                sx={{ bgcolor: "black", color: "white" }}
+              >
+                Q.なぜプログラマーですか？
+              </AccordionSummary>
+              <AccordionDetails>
+                私は幼い頃からプログラマーになりたいという夢がありました。
+                <br />
+                また、海外の生活にも憧れを抱いていました。
+                <br />
+                自分がやりたいことを追求しながらも、より社会に貢献し、
+                <br />
+                つまり人間の生活を便利にするプログラムを作ろうと思っております。
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+                sx={{ bgcolor: "black", color: "white" }}
+              >
+                Q.強みは何ですか？
+              </AccordionSummary>
+              <AccordionDetails>
+                私の強みは次のとおりです。 <br />
+                ・コーディング:EC、B2C、B2B
+                Saasソリューションなど多様な分野のプロジェクト経験
+              </AccordionDetails>
+            </Accordion>
+            <Box sx={buttonContainerStyle}>
+              <Button onClick={handleClose} sx={{ mt: 2 }}>
+                CLOSE
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Modal>
